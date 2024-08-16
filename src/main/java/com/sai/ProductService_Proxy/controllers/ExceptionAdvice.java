a77d411fd1d7056eb1855633216065cd7ce771c8
Babloo007 <msvreddy77@gmail.com>
2024-08-16 19:26:29 +0530

    class 190 commit 2

---
+++ src/main/java/com/sai/ProductService_Proxy/controllers/ExceptionAdvice.java
@@ -0,0 +1,15 @@
+package com.sai.ProductService_Proxy.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<String> handleException(Exception e) {
+        return new ResponseEntity<>("Exception message from exception advice class", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
